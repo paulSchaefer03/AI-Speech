c70bc5cf45a55673fcf13e7f0d1bcd7b89cbc73a
paulSchaefer03 <paul.schaefer@cs.hs-fulda.de>
2025-06-28 23:34:46 +0200

    Test Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { ApplicationShell } from "@/components/layout/ApplicationShell";
 import { Providers } from "@/contexts/Providers";
 import "@/styles/App.css";
-
+//Eine App-Komponente, die die Hauptanwendung darstellt und die Anwendungsschale sowie die Kontext-Provider einbindet.
 function App() {
   return (
     <div className="App">
